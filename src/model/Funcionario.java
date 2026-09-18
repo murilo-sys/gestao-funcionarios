dc5349e59c7aa7ca4af6dd2c3750eab86ee318bd
murilo-sys <murilod_santiago@hotmail.com>
2026-09-17 22:15:16 -0300

    feat: setup project structure and model classes (Pessoa and Funcionario)

---
+++ src/model/Funcionario.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+    
+    // Declaração dos atributes de Funcionario extendendo de pessoa (nome e dataNascimento)
+    private BigDecimal salario;
+    private String funcao;
+
+    // Construtor recebendo atributos
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao){
+        // Passa os atributes nome e dataNascimento para a classe herdada
+        super(nome, dataNascimento);
+
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+}
---
+++ src/model/Pessoa.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    
+    // Declaração dos atributos de Pessoa
+    private String nome;
+    private LocalDate dataNascimento;
+
+    // Construtor recebendo nome e dataNascimento como parâmetro
+    public Pessoa(String nome, LocalDate dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // Metodo getNome
+    public String getNome(){
+        return nome;
+    }
+
+     // Metodo getNome
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+}
